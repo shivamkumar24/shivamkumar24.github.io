7fe2f3648fbd633f484eaa59a9c9db0f39bfee03
Shivam <shivam2432000@gmail.com>
2023-01-03 17:32:15 +0530

    This is portfolio code

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,122 @@
-import React from "react";
+import { Box, Button, Image } from "@chakra-ui/react";
 
-const Home = () => {
-  return <div>Home</div>;
-};
+export default function Home() {
+  const property = {
+    imageUrl: "https://avatars.githubusercontent.com/u/76693614?v=4",
+    imageAlt: "Shivam Profile Picture",
+  };
 
-export default Home;
+  return (
+    <>
+      {/* First Container */}
+      <Box
+        display="flex"
+        pt={{ base: "20px", md: "100px" }}
+        pb={{ base: "20px" }}
+        mb={{ base: "30px", md: "50px" }}
+        justifyContent={{ base: "center", md: "space-around" }}
+        alignItems={{ base: "center" }}
+        flexDirection={{ base: "column", md: "row" }}
+      >
+        <Box p="6">
+          <Box
+            fontWeight="bold"
+            fontSize={{ base: "15px", md: "30px" }}
+            // textAlign={{ base: "center" }}
+          >
+            Hi, I am
+          </Box>
+          <Box
+            fontWeight="bold"
+            fontSize={{ base: "22px", md: "45px" }}
+            // textAlign={{ base: "center" }}
+          >
+            Shivam Kumar
+          </Box>
+          <Box
+            fontWeight="bold"
+            fontSize={{ base: "15px", md: "30px" }}
+            // textAlign={{ base: "center" }}
+            color="red"
+          >
+            Full Stack Web Developer
+          </Box>
+          <Box margin="10px">
+            <Button
+              display={"inline-flex"}
+              fontSize={{ base: "sm", md: "md" }}
+              fontWeight={"bold"}
+              letterSpacing="2px"
+              color={"white"}
+              bg={"red"}
+              href={"#"}
+              _hover={{
+                bg: "red.500",
+              }}
+            >
+              Resume
+            </Button>
+          </Box>
+        </Box>
+
+        <Image
+          src={property.imageUrl}
+          alt={property.imageAlt}
+          borderRadius="50%"
+          height={{ base: "200px", md: "300px" }}
+          width={{ base: "200px", md: "300px" }}
+        />
+      </Box>
+
+      {/* Second Container */}
+      <Box>
+        <Box
+          fontWeight="bold"
+          fontSize={{ base: "22px", md: "45px" }}
+          textAlign={{ base: "center" }}
+          textDecoration="underline"
+        >
+          Who I am
+        </Box>
+        <Box
+          width="90%"
+          margin="auto"
+          display="flex"
+          pt={{ base: "20px", md: "30px" }}
+          pb={{ base: "20px" }}
+          mb={{ base: "30px", md: "50px" }}
+          justifyContent={{ base: "center", md: "space-around" }}
+          alignItems={{ base: "center" }}
+          flexDirection={{ base: "column", md: "row" }}
+        >
+          <Image
+            src="https://t3.ftcdn.net/jpg/00/79/32/70/240_F_79327071_ubG3dWNUwUeVFqALg55FLqGpJFgfnoRY.jpg"
+            alt="aboutUs"
+            borderRadius="20px"
+            height={{ base: "200px", md: "300px" }}
+            width={{ base: "200px", md: "500px" }}
+          />
+          <Box margin="20px">
+            <Box
+              fontSize={{ base: "15px", md: "22px" }}
+              mb="8px"
+              fontWeight="semibold"
+            >
+              My name is Shivam Kumar. I am from East Champaran, Bihar ( India )
+              and I enjoy creating things that live on the internet.
+            </Box>
+            <Box fontSize={{ base: "12px", md: "18px" }} mb="8px">
+              I am a passionate and self-motivated Aspiring Full-Stack Develoer
+              from Bihar (India). Seeking challenging oppurtunities to make the
+              best use of my abilities and knowledge to develop both my
+              professional and interpersonal skills, along with fulfilling the
+              organization's growth and goals.
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+
+      {/* Third Container */}
+    </>
+  );
+}
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,101 @@
-import React from "react";
+import {
+  Box,
+  Flex,
+  HStack,
+  Link,
+  IconButton,
+  Button,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 
-const Navbar = () => {
+const Links = ["Home", "About Me", "Skills", "Projects", "Contact"];
+
+const NavLink = ({ children }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={"md"}
+    _hover={{
+      textDecoration: "none",
+      bg: "red.800",
+      color: "white",
+    }}
+    color="black"
+    fontWeight="bold"
+    fontSize="18px"
+    href={"/"}
+  >
+    {children}
+  </Link>
+);
+
+export default function Navbar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
-  );
-};
+    <>
+      <Box bg={useColorModeValue("purple.300", "black.900")} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={"center"}>
+            <Box
+              fontSize={{ base: "md", md: "30px" }}
+              fontWeight="bold"
+              fontStyle="italic"
+              cursor="pointer"
+              color="red"
+            >
+              Shivam Kumar
+            </Box>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+          </HStack>
+          <Flex alignItems={"center"}>
+            <Button
+              display={"inline-flex"}
+              fontSize={"md"}
+              fontWeight={"bold"}
+              letterSpacing="2px"
+              color={"white"}
+              bg={"red.600"}
+              href={"#"}
+              _hover={{
+                bg: "pink.300",
+              }}
+            >
+              Resume
+            </Button>
+          </Flex>
+        </Flex>
 
-export default Navbar;
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+
+      {/* <Box p={4}>Main Content Here</Box> */}
+    </>
+  );
+}
