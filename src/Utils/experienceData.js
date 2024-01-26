09e049781b27a33940b2375ab3720a599845c0e8
Shivam Kumar <shivam2432000@gmail.com>
2024-01-26 12:16:18 +0530

    This is resume update

--- src/Utils/experienceData.js
+++ src/Utils/experienceData.js
@@ -3,7 +3,7 @@ const experienceData = [
     role: "SDE-Intern",
     location: "Remote",
     start_date: "Sep 2023",
-    end_date: "Feb 2024",
+    end_date: "Current",
     company_name: "Masai School",
     works: [
       "Developed and maintained web applications using languages such as HTML, CSS, JavaScript, ReactJS, TypeScript, Redux and Chakra-ui.",
