2f005d9362d69e17b8fc1c51eba2bf10f6e14168
Shivam Kumar <shivam2432000@gmail.com>
2024-04-13 10:51:15 +0530

    This is update resume and experience data

--- src/Utils/experienceData.js
+++ src/Utils/experienceData.js
@@ -3,7 +3,7 @@ const experienceData = [
     role: "SDE-Intern",
     location: "Remote",
     start_date: "Sep 2023",
-    end_date: "Present",
+    end_date: "Mar 2024",
     company_name: "Nolan Edutech Private Limited",
     works: [
       "Collaborated with cross-functional teams on various software development projects.",
